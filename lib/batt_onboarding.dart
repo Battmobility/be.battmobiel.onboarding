ea3f5d3c533b8af7bd9be20c5c863dcaea1b9dc2
Kris <kris.boonefaes@gmail.com>
2025-01-06 12:09:35 +0100

    expose main form

--- lib/batt_onboarding.dart
+++ lib/batt_onboarding.dart
@@ -5,4 +5,5 @@ library;
 
 export 'src/batt_onboarding_base.dart';
 export 'src/presentation/batt_onboarding_ui.dart';
+export 'src/presentation/onboarding_landing_form.dart';
 // TODO: Export any libraries intended for clients of this package.
